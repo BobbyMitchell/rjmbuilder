f0a1f7c70c8d15a5831d5bc445adbec0a057d4c3
rob <robmitchell@email.com>
2018-02-15 10:11:25 +0100

    Added basic controllers and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'services/index'
+
+  get 'services/show'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root to: 'services/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
