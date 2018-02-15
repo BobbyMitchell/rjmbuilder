7a69cb87c671a2bc610e3ec3f68d2ec97d9158c0
rob <robmitchell@email.com>
2018-02-15 20:33:38 +0100

    seed completed, show and index working with pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,13 @@ Service.destroy_all
 
 urls = [
 
-  "http://res.cloudinary.com/robmitchell/image/upload/v1518718003/fencing_1_hif3y3.jpg"
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518718003/fencing_1_hif3y3.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/fencing_2_woplah.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/fencing_3_r7y5th.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/fencing_4_yvsexl.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/fencing_5_ros5ux.jpg"
 ]
+
 service1 = Service.new(
   name: "fencing",
   text: "Bla bla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla bla
@@ -16,7 +21,11 @@ service1.photo_urls = urls
 
 urls = [
 
-  "http://res.cloudinary.com/robmitchell/image/upload/v1518718003/roofing_1_dhnq8a.jpg"
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518718003/roofing_1_dhnq8a.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721198/roofing_2_rn59yc.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721198/roofing_3_zvx5xg.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721198/roofing_4_nxnq6s.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721198/roofing_5_ogiteg.jpg"
 ]
 service2 = Service.new(
   name: "roofing",
@@ -30,7 +39,11 @@ service2.photo_urls = urls
 
 urls = [
 
-  "http://res.cloudinary.com/robmitchell/image/upload/v1518718003/adaptation_1_ecbyqa.jpg"
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518718003/adaptation_1_ecbyqa.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721196/adaptation_2_e2wgg8.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721196/adaptation_3_bgv6ds.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721196/adaptation_4_sctuih.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721196/adaptation_5_ni1jcq.jpg"
 ]
 service3 = Service.new(
   name: "adaptations",
@@ -44,7 +57,11 @@ service3.photo_urls = urls
 
 urls = [
 
- "http://res.cloudinary.com/robmitchell/image/upload/v1518718004/extention_1_vqbcqq.jpg"
+ "http://res.cloudinary.com/robmitchell/image/upload/v1518718004/extention_1_vqbcqq.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721196/extention_2_vw0ldl.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721196/extention_3_hwiish.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/extention_4_hbb0sc.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/extention_5_kcwhlk.jpg"
 ]
 service4 = Service.new(
   name: "extentions",
@@ -58,7 +75,11 @@ service4.photo_urls = urls
 
 urls = [
 
-  "http://res.cloudinary.com/robmitchell/image/upload/v1518718004/newbuild_1_hxemtw.jpg"
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518718004/newbuild_1_hxemtw.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/newbuild_2_kuwqkh.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721198/newbuild_3_q1s82d.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721198/newbuild_4_vg2kuz.jpg",
+  "http://res.cloudinary.com/robmitchell/image/upload/v1518721197/newbuild_5_bbm6n4.jpg"
 ]
 service5 = Service.new(
   name: "newbuild",
