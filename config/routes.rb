1fb850d5d2b67480c5f5d7b47cc5f2a6f8358085
rob <robmitchell@email.com>
2018-03-13 07:44:12 +0100

    Contact for ser up following instuctions from http://simple-notes.info/create-contact-us-form-in-rails-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+resources 'contacts', only: [:new, :create], path_names: { new: '' }
+if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+end
+
   resources :services, only: [:index, :show]
   mount Attachinary::Engine => "/attachinary"
   get 'pages/about'
