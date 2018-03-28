2ab776023d7856b6c07278dfbb3b6fdbffceefb4
rob <robmitchell@email.com>
2018-03-28 15:06:55 +0200

    Photos in gallery but text from unknown reason at bottom

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,6 +7,11 @@ class ServicesController < ApplicationController
     @service = Service.find(params[:id])
   end
 
+  def gallery
+       @services = Service.all
+
+  end
+
   def service_params
     params.require(:service).permit(:name, :text, photos: [])
   end
